aec5f888abc9f52a4b280fdc3a4983690f29cf51
wdmdevelopment <wavesdevelopmentteam@gmail.com>
2023-07-12 15:14:24 +0530

    new

--- hotelbooking/src/main/java/com/booking/utils/ImageUtils.java
+++ hotelbooking/src/main/java/com/booking/utils/ImageUtils.java
@@ -7,21 +7,19 @@ import org.springframework.web.multipart.MultipartFile;
 import org.webjars.NotFoundException;
 
 public class ImageUtils {
-	
 
 	public static File uploadFile(MultipartFile file, String folderPath) {
 		try {
-		 File path = new File(folderPath + file.getOriginalFilename());
-         path.createNewFile();
-         FileOutputStream output = new FileOutputStream(path);
-         output.write(file.getBytes());
-         output.close();
-		
-		return path;
-		}catch (Exception e) {
-		throw new NotFoundException(e.getMessage());
+			File path = new File(folderPath + file.getOriginalFilename());
+			path.createNewFile();
+			FileOutputStream output = new FileOutputStream(path);
+			output.write(file.getBytes());
+			output.close();
+
+			return path;
+		} catch (Exception e) {
+			throw new NotFoundException(e.getMessage());
 		}
-		
 	}
-	
+
 }
